539668710298b504edcb76bdcdb768b385e53685
ilya Lebedyuk <laplasianin@gmail.com>
2017-06-28 16:45:57 +0300

    'fix'

--- src/main/groovy/com/laplasianin/jirastatusupdate/utils/HttpUtils.groovy
+++ src/main/groovy/com/laplasianin/jirastatusupdate/utils/HttpUtils.groovy
@@ -3,7 +3,7 @@ package com.laplasianin.jirastatusupdate.utils
 class HttpUtils {
 
     static final String runGet(config, name, params = '') {
-        def string = "curl -u ${config.jiraUsername}:${config.jira{Password} -X GET -H \"Accept: application/json\" ${config.baseUrl}${config.restApi}${name}${params ?: ''}"
+        def string = "curl -u ${config.jiraUsername}:${config.jiraPassword} -X GET -H \"Accept: application/json\" ${config.baseUrl}${config.restApi}${name}${params ?: ''}"
         println("Executing ${URLDecoder.decode(string)}")
         return string.execute().text
     }
